574f57d6b6979b2426d8f5b1bcf6ae76c6450e04
Alejandro Sanchez <aalejo@gmail.com>
2023-04-26 22:58:30 +0000

    added small fix for codespaces to pointto 0.0.0.0 instead of localhost

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var app = express();
 var compiler = webpack(config);
 
 var serverPort = process.env.PORT || 3000;
+let host = `localhost`;
+
+//only for codespaces
+if(process.env.CODESPACE_NAME){
+  host = `0.0.0.0`;
+}
 
 app.use(require("webpack-dev-middleware")(compiler, {
   publicPath: config.output.publicPath
@@ -20,11 +26,11 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(serverPort, process.env.IP || "localhost", function (err) {
+app.listen(serverPort, process.env.IP || host, function (err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log("Listening at http://localhost:" + serverPort);
+  console.log("Listening at http://"+host+":" + serverPort);
 });
